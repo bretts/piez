c258122a2d2e807fdbb6992cd3598a14a09f6202
Brett Sykes <bsykes@akamai.com>
2016-07-18 10:40:33 -0400

    Remove pageref and use onNavigated.

--- devtools/devtoolscontent.js
+++ devtools/devtoolscontent.js
@@ -1,23 +1,22 @@
 var PiezController = {}
 
 window.onload = function() {
-	PiezController.pageref              = 'pageref_0';
 	PiezController.current_page         = new Page();
 	PiezController.current_display_mode = "piezModeSimple";
 
+	chrome.devtools.network.onNavigated.addListener(function(http_transaction) {
+		PiezController.current_page = new Page();
+
+		chrome.extension.sendMessage({
+			type: "use-piez-on-page"
+		});
+
+		chrome.storage.local.get("piezCurrentState", function(state) {
+			PiezController.current_display_mode = state["piezCurrentState"];
+		 });
+	});
+
 	chrome.devtools.network.onRequestFinished.addListener(function(http_transaction) {
-		if(http_transaction.pageref != PiezController.pageref) {
-
-			chrome.extension.sendMessage({
-				type: "use-piez-on-page"
-			});
-
-			chrome.storage.local.get("piezCurrentState", function(state) {
-				PiezController.current_page         = new Page();
-				PiezController.pageref              = http_transaction.pageref;
-				PiezController.current_display_mode = state["piezCurrentState"];
-			 });
-		}
 		parseResult(http_transaction, PiezController.current_page);
 		displayResult(PiezController.current_page, PiezController.current_display_mode);
 	});

0337ab2d244db994fe3d74f91c2840389b04afff
Paul Bartlett <bartlett_paul@hotmail.com>
2019-01-30 12:56:40 -0500

    Revert minor autoformatting

--- background.js
+++ background.js
@@ -144,13 +144,11 @@ var getCookiesUrl = function (href) {
 	return (link.protocol + "//" + link.hostname);
 };
 
-var logUrlAnalytics = function (tab) {
-	(function (i, s, o, g, r, a, m) {
-	i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-		(i[r].q = i[r].q || []).push(arguments)
-	}, i[r].l = 1 * new Date(); a = s.createElement(o),
-		m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-	})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+var logUrlAnalytics = function(tab) {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 	ga('create', 'UA-62551710-2', 'auto');
 	ga('set', 'checkProtocolTask', function () { });
